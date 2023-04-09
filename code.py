7dbae2b3d8f41e15cd722c514e45879ab5216496
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 00:49:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -34,37 +34,18 @@ def en_command_handler(_, message: Message):
     message.reply_text(f"Your image ID is {image_id}.")
 
 
-
 @app.on_message(filters.command("dy"))
 def dy_command_handler(_, message: Message):
     # ask the user for the image ID
     message.reply_text("Please enter the image ID:")
 
-    # create an inline keyboard with a "Cancel" button
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Cancel", callback_data="cancel")]
-    ])
-
-    # send the message with the keyboard
-    app.send_message(
-        chat_id=message.chat.id,
-        text="Please enter the image ID:",
-        reply_markup=keyboard
-    )
-
-
     # set the next handler to get the image ID from the user
-    app.register_callback_query_handler(handle_image_id_1, message.chat.id)
+    app.register_next_step_handler(message, handle_image_id)
 
 
-def handle_image_id_1(_, query: CallbackQuery):
-    # check if the callback query data is "cancel"
-    if query.data == "cancel":
-        query.message.reply_text("Cancelled.")
-        return
-
-    # get the image ID from the callback query data
-    image_id = query.data
+def handle_image_id(message: Message):
+    # get the image ID from the user's message
+    image_id = message.text.strip()
 
     # look up the image in MongoDB
     db = Database()
@@ -72,29 +53,11 @@ def handle_image_id_1(_, query: CallbackQuery):
 
     # check if the image exists
     if not image:
-        query.answer("Image not found.", show_alert=True)
+        message.reply_text("Image not found.")
         return
 
     # send the image to the user
-    query.message.reply_photo(photo=image["file_id"])
-
-
-@app.on_callback_query()
-def handle_callback_query(_, query: CallbackQuery):
-    # get the image ID from the callback data
-    image_id = query.data.strip()
-
-    # look up the image in MongoDB
-    db = Database()
-    image = db.find_document_by_id("images", image_id)
-
-    # check if the image exists
-    if not image:
-        query.answer("Image not found.", show_alert=True)
-        return
-
-    # send the image to the user
-    query.message.reply_photo(photo=image["file_id"])
+    app.send_photo(chat_id=message.chat.id, photo=image["file_id"])
 
 
 if __name__ == "__main__":
