3fd7df800ad744c44ce9f4ea0cae1c19aeb760ba
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 00:12:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
 BOT_TOKEN = "5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk"
 
 # MongoDB connection settings
-MONGO_URI = "mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_URI = "mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"
 MONGO_DB_NAME = "telegram_bot"
 MONGO_COLLECTION_NAME = "images"
