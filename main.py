af3ffd05fc6a8eae010cc62140989cb66e271949
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 03:53:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import schedule
 import time
 
 from code import updater
+from batch import batch_command_handler
 from reset import reset_handler
 from database import Database
 
@@ -16,6 +17,10 @@ if __name__ == "__main__":
 
     # schedule the database reset
     schedule.every().day.at("00:00").do(reset_database)
+    
+    # Define the /batch command handler
+    batch_handler = CommandHandler("batch", batch_command_handler)
+    dispatcher.add_handler(batch_handler)
 
     # add the reset command handler
     updater.dispatcher.add_handler(reset_handler)
