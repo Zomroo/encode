25b7fe535ea1e5ff8edf4ed029096e6ddca8f784
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-09 19:04:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,112 +1,76 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from io import BytesIO
 from PIL import Image
-import random
-import io
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
 
-# Change the following key to your own secret key
-KEY = b'mysecretpassword'
-
-TOKEN = '5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk'
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a bot that can encrypt images. To encrypt an image, send me an image and use the /en command.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Image Encryption Bot!")
+
 
 def encrypt_image(update, context):
-    # Check if the message is a reply and contains an image
-    if not update.message.reply_to_message or not update.message.reply_to_message.photo:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to an image message with /en to encrypt.")
-        return
+    # Check if the reply message is an image
+    if update.message.reply_to_message.photo:
+        # Get the image file_id
+        file_id = update.message.reply_to_message.photo[-1].file_id
+        # Get the image file object
+        file_obj = context.bot.get_file(file_id)
+        # Download the image data
+        img_bytes = file_obj.download_as_bytearray()
+        # Encrypt the image data
+        img_data = bytearray([b ^ 27 for b in img_bytes])
+        # Create a Pillow image object
+        img = Image.open(BytesIO(img_data))
+        # Send the encrypted image
+        bio = BytesIO()
+        img.save(bio, format="PNG")
+        bio.seek(0)
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=bio)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to an image with the /en command.")
 
-    # Get the file ID of the largest version of the photo in the replied message
-    file_id = update.message.reply_to_message.photo[-1].file_id
-
-    # Download the image
-    file = context.bot.get_file(file_id)
-    img_bytes = io.BytesIO(file.download_as_bytearray())
-
-    # Open the image from the byte stream
-    img = Image.open(img_bytes)
-
-    # Get the dimensions of the image
-    width, height = img.size
-
-    # Convert the image to a byte stream
-    img_bytes = io.BytesIO()
-    img.save(img_bytes, format='JPEG')
-    img_bytes.seek(0)
-
-    # Pad the image bytes to be a multiple of the AES block size
-    padded_img_bytes = pad(img_bytes.read(), AES.block_size)
-
-    # Generate a random initialization vector (IV)
-    iv = bytes([random.randint(0, 255) for i in range(AES.block_size)])
-
-    # Encrypt the padded image bytes using AES in CBC mode with the IV and key
-    cipher = AES.new(KEY, AES.MODE_CBC, iv)
-    encrypted_img_bytes = iv + cipher.encrypt(padded_img_bytes)
-
-    # Send the encrypted image
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=io.BytesIO(encrypted_img_bytes))
 
 def decrypt_image(update, context):
-    # Check if the message is a reply and contains an image
-    if not update.message.reply_to_message or not update.message.reply_to_message.photo:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to an image message with /dy to decrypt.")
-        return
+    # Check if the reply message is an image
+    if update.message.reply_to_message.photo:
+        # Get the image file_id
+        file_id = update.message.reply_to_message.photo[-1].file_id
+        # Get the image file object
+        file_obj = context.bot.get_file(file_id)
+        # Download the image data
+        img_bytes = file_obj.download_as_bytearray()
+        # Decrypt the image data
+        img_data = bytearray([b ^ 27 for b in img_bytes])
+        # Create a Pillow image object
+        img = Image.open(BytesIO(img_data))
+        # Send the decrypted image
+        bio = BytesIO()
+        img.save(bio, format="PNG")
+        bio.seek(0)
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=bio)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to an image with the /dy command.")
 
-    # Get the file ID of the largest version of the photo in the replied message
-    file_id = update.message.reply_to_message.photo[-1].file_id
-
-    # Download the image
-    file = context.bot.get_file(file_id)
-    encrypted_img_bytes = io.BytesIO(file.download_as_bytearray()).read
-
-    # Get the initialization vector (IV) and encrypted image data from the byte stream
-    iv = encrypted_img_bytes[:AES.block_size]
-    encrypted_img_data = encrypted_img_bytes[AES.block_size:]
-
-    # Decrypt the image using AES in CBC mode
-    cipher = AES.new(KEY, AES.MODE_CBC, iv)
-    padded_img_data = cipher.decrypt(encrypted_img_data)
-
-    # Unpad the decrypted image
-    unpadder = unpad(bytes(padded_img_data), AES.block_size)
-    img_data = unpadder.update(padded_img_data) + unpadder.finalize()
-
-    # Create a new image with the same dimensions and the decrypted pixel values
-    img = Image.open(io.BytesIO(img_data))
-    width, height = img.size
-
-    # Save the decrypted image to a buffer
-    buffer = io.BytesIO()
-    img.save(buffer, format='JPEG')
-    buffer.seek(0)
-
-    # Send the decrypted image
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=buffer)
- 
 
 def main():
-    print("Starting bot...")
-    # Create the Updater and pass it the bot's token
-    updater = Updater(TOKEN, use_context=True)
-
-    # Add handlers for the /start, /en, and /dy commands
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('en', encrypt_image))
-    updater.dispatcher.add_handler(CommandHandler('dy', decrypt_image))
-
+    # Create a Telegram Bot API object
+    token = "YOUR_TOKEN_HERE"
+    bot = telegram.Bot(token=token)
+    # Create a Telegram Bot API updater
+    updater = Updater(token=token, use_context=True)
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+    # Add command handlers
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("en", encrypt_image))
+    dp.add_handler(CommandHandler("dy", decrypt_image))
     # Start the bot
-    try:
-        updater.start_polling()
-    except Exception as e:
-        print(f"Error starting bot: {e}")
+    updater.start_polling()
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
-print("Bot started.")
 
 if __name__ == '__main__':
     main()
-
