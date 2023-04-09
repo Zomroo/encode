f62fcb34e025b69379900797ffd15219281be534
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 04:00:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import schedule
 import time
 
 from telegram.ext import CommandHandler
+from telegram.ext import Updater
 from code import updater
 from batch import batch_command_handler
 from reset import reset_handler
 from database import Database
+
+# Create an Updater object and pass it your bot's token
+updater = Updater(token='YOUR_TOKEN_HERE', use_context=True)
+
+# Get the dispatcher object from the updater
+dispatcher = updater.dispatcher
 
 def reset_database():
     db = Database()
@@ -22,6 +29,14 @@ if __name__ == "__main__":
     # Define the /batch command handler
     batch_handler = CommandHandler("batch", batch_command_handler)
     dispatcher.add_handler(batch_handler)
+    
+    
+    
+    dispatcher.add_handler(CommandHandler('batch', batch_command_handler))
+    dispatcher.add_handler(CommandHandler('done', done_command_handler))
+    dispatcher.add_handler(CommandHandler('dy', dy_command_handler))
+    dispatcher.add_handler(MessageHandler(Filters.photo, image_handler))
+   
 
     # add the reset command handler
     updater.dispatcher.add_handler(reset_handler)
