e4ea68de9c6458df478b5c00b75acae1c124595b
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 02:10:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = 16844842
 API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
 
 # Telegram Bot API token
-BOT_TOKEN = "5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk"
+BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
 
 # MongoDB connection settings
 MONGODB_URI = "mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"
