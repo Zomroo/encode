39da1497230d855bd9e24aad3c888ea2da86850d
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 02:27:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ import schedule
 import time
 
 from code import updater
-from reset import updater as reset_updater  # import the reset_updater from reset.py
+from reset import reset_handler, updater as reset_updater
 from database import Database
-
-# Start the reset bot to register the reset command
-reset_updater.start_polling()
 
 # Start the main bot
 updater.start_polling()
 
-# Schedule the database reset
+# Define the reset database function
 def reset_database():
-    reset_updater.start_polling()
     db = Database()
     db.client.drop_database(db.db.name)
-    reset_updater.stop()
     print("Database reset.")
 
+# Schedule the database reset
 schedule.every().day.at("00:00").do(reset_database)
 
+# Run the scheduled tasks
 while True:
     schedule.run_pending()
     time.sleep(1)
+    
+    # Check for the "/reset" command
+    updates = reset_updater.polling_queue.get()
+    for update in updates:
+        reset_handler(update, reset_updater)
