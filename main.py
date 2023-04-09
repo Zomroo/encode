c46dba563ca7c5d30bb4922c432edeceea45f64a
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 02:07:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from code import updater
 from database import Database
 from reset import updater as reset_updater
 
-# start the reset bot to register the reset command
-reset_updater.start_polling()
 
 def reset_database():
     db = Database()
@@ -15,6 +13,9 @@ def reset_database():
 
 
 if __name__ == "__main__":
+    # start the reset bot to register the reset command
+    reset_updater.start_polling()
+
     # start the main bot
     updater.start_polling()
 
@@ -22,5 +23,8 @@ if __name__ == "__main__":
     schedule.every().day.at("00:00").do(reset_database)
 
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        try:
+            schedule.run_pending()
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
