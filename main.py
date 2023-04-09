fe18eb1bd20e07da38054d36053db6d613a3e5fd
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 02:15:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import schedule
 import time
+import psutil
 
 from code import updater
 from database import Database
 from reset import updater as reset_updater
 
+def is_running(process):
+    for proc in psutil.process_iter():
+        try:
+            if process.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
 
 def reset_database():
     db = Database()
     db.client.drop_database(db.db.name)
     print("Database reset.")
 
-
 if __name__ == "__main__":
+    if is_running("python main.py"):
+        print("Bot is already running!")
+        exit(1)
+
     # start the reset bot to register the reset command
     reset_updater.start_polling()
 
