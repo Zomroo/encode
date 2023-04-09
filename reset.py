47b292e0c3cc3d02119d4dac8217f37292698499
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 03:27:56 +0530

    Update reset.py

--- reset.py
+++ reset.py
@@ -1,8 +1,6 @@
 from telegram.ext import Updater, CommandHandler
 from database import Database
-
 from config import BOT_TOKEN, AUTHORIZED_USERS
-
 
 # Create an Updater object and pass in the bot's token
 updater = Updater(token=BOT_TOKEN, use_context=True)
@@ -10,12 +8,11 @@ dispatcher = updater.dispatcher
 
 # Define the reset command handler
 def reset_command_handler(update, context):
-    user_id = update.message.from_user.id
+    user_id = update.effective_user.id
     if user_id in AUTHORIZED_USERS:
         db = Database()
         db.client.drop_database(db.db.name)
         context.bot.send_message(chat_id=update.effective_chat.id, text="Database reset.")
-    else:
 
 # Add the reset command handler
 reset_handler = CommandHandler("reset", reset_command_handler)
