54dbd7f2a7ce058d4f78fc45ac5a2cfe28cc98a7
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 05:03:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import schedule
 import time
-import config
 
 from code import updater
 from reset import reset_handler
-from database import Database
 from batch import batch_command_handler, done_command_handler, image_handler
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
-
-# Create an Updater object and pass in the bot's token
-updater = Updater(token=config.BOT_TOKEN, use_context=True)
-
-# Create a Dispatcher object
-dispatcher = updater.dispatcher
-
+from database import Database
 
 def reset_database():
     db = Database()
@@ -27,12 +18,12 @@ if __name__ == "__main__":
     # schedule the database reset
     schedule.every().day.at("00:00").do(reset_database)
     
-    # Define the /batch command handler
-    batch_handler = CommandHandler("batch", batch_command_handler)
-    dispatcher.add_handler(batch_handler)
+    # Define the /batch command handler	
+    batch_handler = CommandHandler("batch", batch_command_handler)	
+    dispatcher.add_handler(batch_handler)	
 
-    dispatcher.add_handler(CommandHandler('done', done_command_handler))
-    dispatcher.add_handler(CommandHandler('dy', dy_command_handler))
+    dispatcher.add_handler(CommandHandler('done', done_command_handler))	
+    dispatcher.add_handler(CommandHandler('dy', dy_command_handler))	
     dispatcher.add_handler(MessageHandler(Filters.photo, image_handler))
 
     # add the reset command handler
