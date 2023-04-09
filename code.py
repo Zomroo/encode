6a39b136ff42182a0f44776c3c3c6090eaec36fb
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 01:01:53 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,70 +2,81 @@ import os
 import re
 import uuid
 
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import BOT_TOKEN
 from database import Database
 
-app = Client("my_bot", bot_token=BOT_TOKEN)
+# Create an Updater object and pass in the bot's token
+updater = Updater(token=BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
 
-@app.on_message(filters.command("start"))
-def start_command_handler(_, message: Message):
-    message.reply_text("Welcome to my bot!")
+# Define the start command handler
+def start_command_handler(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!")
 
 
-@app.on_message(filters.command("en") & filters.reply)
-def en_command_handler(_, message: Message):
-    # check if the reply message is an image
-    if not message.reply_to_message.photo:
-        message.reply_text("Please reply with an image.")
+# Define the en command handler
+def en_command_handler(update, context):
+    # Check if the reply message is an image
+    if not update.message.reply_to_message.photo:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply with an image.")
         return
 
-    # generate a unique ID for the image
+    # Generate a unique ID for the image
     image_id = str(uuid.uuid4())[:7]
 
-    # save the image to MongoDB
+    # Save the image to MongoDB
     db = Database()
-    db.insert_document("images", {"_id": image_id, "file_id": message.reply_to_message.photo.file_id})
+    db.insert_document("images", {"_id": image_id, "file_id": update.message.reply_to_message.photo[-1].file_id})
 
-    # send a reply to the user with the ID
-    message.reply_text(f"Your image ID is {image_id}.")
-
-
-@app.on_message(filters.command("dy"))
-def dy_command_handler(_, message: Message):
-    # ask the user for the image ID
-    message.reply_text("Please enter the image ID:")
-
-    # start the callback handler
-    @app.on_callback_query(filters.regex(r'^\w{7}$'))
-async def callback_handler(_, query: CallbackQuery):
-    await query.answer(text="This is a callback query!")
+    # Send a reply to the user with the ID
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Your image ID is {image_id}.")
 
 
+# Define the dy command handler
+def dy_command_handler(update, context):
+    # Ask the user for the image ID
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter the image ID:")
 
-def callback_handler(client, callback_query: CallbackQuery):
-    # get the image ID from the callback data
-    image_id = callback_query.data
+    # Start the callback handler
+    def callback_handler(update, context):
+        query = update.callback_query
+        # Get the image ID from the callback data
+        image_id = query.data
 
-    # look up the image in MongoDB
-    db = Database()
-    image = db.find_document_by_id("images", image_id)
+        # Look up the image in MongoDB
+        db = Database()
+        image = db.find_document_by_id("images", image_id)
 
-    # check if the image exists
-    if not image:
-        client.answer_callback_query(callback_query.id, text="Image not found.")
-        return
+        # Check if the image exists
+        if not image:
+            context.bot.answer_callback_query(callback_query_id=query.id, text="Image not found.")
+            return
 
-    # send the image to the user
-    client.send_photo(chat_id=callback_query.message.chat.id, photo=image["file_id"])
+        # Send the image to the user
+        context.bot.send_photo(chat_id=query.message.chat_id, photo=image["file_id"])
 
-    # end the conversation
-    client.answer_callback_query(callback_query.id, text="Here is your image.")
+        # End the conversation
+        context.bot.answer_callback_query(callback_query_id=query.id, text="Here is your image.")
+
+    callback_handler = CallbackQueryHandler(callback_handler, pattern='^\\w{7}$')
+    dispatcher.add_handler(callback_handler)
+
+
+# Add handlers for the start, en and dy commands
+start_handler = CommandHandler("start", start_command_handler)
+en_handler = CommandHandler("en", en_command_handler, filters=Filters.reply)
+dy_handler = CommandHandler("dy", dy_command_handler)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(en_handler)
+dispatcher.add_handler(dy_handler)
 
 
 if __name__ == "__main__":
-    # start the bot
-    app.run()
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
