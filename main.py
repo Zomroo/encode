de1636575c656510d5715aae5d80fe1151b67c68
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-09 22:38:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,11 +27,13 @@ def encrypt_image(message):
         # Encrypt the image by shuffling the pixels
         h, w, c = arr.shape
         flat_arr = arr.reshape(-1, c)
-        random_seed = np.random.randint(1, 100000) # Generate a random seed to use for shuffling
-        np.random.seed(random_seed)
-        np.random.shuffle(flat_arr)
-        arr = flat_arr.reshape(h, w, c)
-        encrypted_img = Image.fromarray(arr)
+        pattern = np.array([[3, 0, 2, 1], [1, 2, 0, 3], [2, 3, 1, 0], [0, 1, 3, 2]]) # Define the pattern for shuffling
+        pattern_flat = pattern.reshape(-1)
+        encrypted_flat_arr = np.zeros_like(flat_arr)
+        for i in range(h*w):
+            encrypted_flat_arr[i] = flat_arr[i][pattern_flat[i % 16]] # Shuffle the pixels according to the pattern
+        encrypted_arr = encrypted_flat_arr.reshape(h, w, c)
+        encrypted_img = Image.fromarray(encrypted_arr)
         
         # Send the encrypted image to the user
         buffered = BytesIO()
@@ -39,8 +41,8 @@ def encrypt_image(message):
         buffered.seek(0)
         bot.send_photo(message.chat.id, photo=buffered)
         
-        # Send the user the random seed used for encryption
-        bot.reply_to(message, f"The random seed used for encryption is {random_seed}. Use this seed with the /dy command to decrypt the image.")
+        # Send the user the pattern used for encryption
+        bot.reply_to(message, f"The pattern used for encryption is {pattern}. Use this pattern with the /dy command to decrypt the image.")
     except Exception as e:
         bot.reply_to(message, "Error: " + str(e))
 
@@ -48,9 +50,10 @@ def encrypt_image(message):
 @bot.message_handler(commands=['dy'])
 def decrypt_image(message):
     try:
-        # Get the photo and random seed from the message
+        # Get the photo and pattern from the message
         photo = message.reply_to_message.photo[-1].file_id
-        random_seed = int(message.text.split()[-1]) # Extract the random seed from the message text
+        pattern_str = message.text.split()[-1] # Extract the pattern from the message text
+        pattern = np.array([int(x) for x in pattern_str.split(",")]).reshape(3,3) # Convert the pattern string to a numpy array
         file_info = bot.get_file(photo)
         downloaded_file = bot.download_file(file_info.file_path)
         
@@ -58,14 +61,19 @@ def decrypt_image(message):
         encrypted_img = Image.open(BytesIO(downloaded_file))
         arr = np.array(encrypted_img)
         
-        # Decrypt the image by unshuffling the pixels
+        # Decrypt the image by unshuffling the pixels in the pattern
         h, w, c = arr.shape
         flat_arr = arr.reshape(-1, c)
-        np.random.seed(random_seed) # Use the same random seed as the encryption algorithm
-        unshuffled_arr = np.zeros_like(flat_arr)
-        for i, j in enumerate(np.argsort(np.random.random(len(flat_arr)))):
-            unshuffled_arr[i] = flat_arr[j] # fix: assign to unshuffled_arr[i] instead of unshuffled_arr[j]
-        arr = unshuffled_arr.reshape(h, w, c)
+        decrypted_flat_arr = np.zeros_like(flat_arr)
+        for i in range(h):
+            for j in range(w):
+                for k in range(c):
+                    new_i, new_j = np.where(pattern == np.array([i % 3, j % 3])) # Find the indices in the pattern that correspond to the current pixel
+                    new_i = new_i[0] + (i // 3) * 3 # Convert the indices to the position of the pixel in the original image
+                    new_j = new_j[0] + (j // 3) * 3
+                    decrypted_flat_arr[new_i*w+new_j][k] = flat_arr[i*w+j][k] # Copy the pixel value to its new position in the decrypted image
+        
+        arr = decrypted_flat_arr.reshape(h, w, c)
         
         # Convert the decrypted image to bytes and send it to the user
         decrypted_img = Image.fromarray(arr)
@@ -77,4 +85,4 @@ def decrypt_image(message):
         bot.reply_to(message, "Error: " + str(e))
 
 
-bot.polling()
+    bot.polling()
