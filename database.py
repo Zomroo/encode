8b4b76ed3b04ad05b8d872bd6450a331bfcaf9da
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 00:01:00 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,30 +1,17 @@
-import datetime
-import pytz
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-from config import MONGO_URI, MONGO_DB_NAME, MONGO_COLLECTION_NAME
+
+from config import MONGODB_URI, MONGODB_NAME
 
 
 class Database:
     def __init__(self):
-        self.client = MongoClient(MONGO_URI)
-        self.db = self.client[MONGO_DB_NAME]
-        self.collection = self.db[MONGO_COLLECTION_NAME]
-    
-    def add_image(self, file_id):
-        return str(self.collection.insert_one({"file_id": file_id}).inserted_id)
-    
-    def get_image_by_id(self, image_id):
-        return self.collection.find_one({"_id": ObjectId(image_id)})
-    
-    def reset(self):
-        self.collection.drop()
-    
-    def schedule_reset(self):
-        tz = pytz.timezone("UTC")
-        now = datetime.datetime.now(tz)
-        tomorrow = now + datetime.timedelta(days=1)
-        tomorrow = datetime.datetime(tomorrow.year, tomorrow.month, tomorrow.day, 0, 0, 0, 0, tz)
-        delta = tomorrow - now
-        delta_seconds = delta.total_seconds()
-        return delta_seconds
+        self.client = MongoClient(MONGODB_URI)
+        self.db = self.client[MONGODB_NAME]
+
+    def insert_document(self, collection_name, document):
+        collection = self.db[collection_name]
+        return collection.insert_one(document)
+
+    def find_document_by_id(self, collection_name, document_id):
+        collection = self.db[collection_name]
+        return collection.find_one({"_id": document_id})
