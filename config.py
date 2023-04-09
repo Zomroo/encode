ded161b465a32faaac9fa3009021984fbc36e2f7
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 03:26:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 # Telegram API credentials
 API_ID = 16844842
 API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
+AUTHORIZED_USERS = [5148561602]  # Replace with the actual user ids of authorized users
 
 # Telegram Bot API token
 BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
