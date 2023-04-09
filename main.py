b0cb04e4b14ddb857e42d75131a60a25f9f429b1
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 04:07:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import schedule
 import time
 
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, MessageHandler, Filters
 from telegram.ext import Updater
 from code import updater
-from batch import batch_command_handler
+from batch import batch_command_handler, done_command_handler, dy_command_handler, image_handler
 from reset import reset_handler
 from database import Database
 
@@ -25,18 +25,14 @@ if __name__ == "__main__":
 
     # schedule the database reset
     schedule.every().day.at("00:00").do(reset_database)
-    
+
     # Define the /batch command handler
     batch_handler = CommandHandler("batch", batch_command_handler)
     dispatcher.add_handler(batch_handler)
-    
-    
-    
-    dispatcher.add_handler(CommandHandler('batch', batch_command_handler))
+
     dispatcher.add_handler(CommandHandler('done', done_command_handler))
     dispatcher.add_handler(CommandHandler('dy', dy_command_handler))
     dispatcher.add_handler(MessageHandler(Filters.photo, image_handler))
-   
 
     # add the reset command handler
     updater.dispatcher.add_handler(reset_handler)
