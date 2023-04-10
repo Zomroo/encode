98f9313a687a45ad5418a1ce8d5896ed24a3721c
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 03:41:34 +0530

    Update reset.py

--- reset.py
+++ reset.py
@@ -1,39 +1,27 @@
-import os
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.contrib import MongoStorage
-
-from config import API_ID, API_HASH, BOT_TOKEN, MONGO_URI, MONGO_DB_NAME, AUTHORIZED_USERS
+from database import Database
+from config import BOT_TOKEN, AUTHORIZED_USERS
 
 # Create a Pyrogram client and pass in the bot's token
-app = Client(
-    "my_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    storage=MongoStorage(
-        uri=MONGO_URI,
-        database_name=MONGO_DB_NAME,
-        client_options={
-            "retryWrites": False
-        }
-    )
-)
+app = Client("my_bot", bot_token=BOT_TOKEN)
 
 # Define the reset command handler
-@app.on_message(filters.command('reset') & filters.user(AUTHORIZED_USERS))
+@app.on_message(filters.command("reset") & filters.user(AUTHORIZED_USERS))
 def reset_command_handler(client, message):
     # Check if the user is authorized to use this command
     if message.from_user.id not in AUTHORIZED_USERS:
         return
 
-    # Delete the entire database
-    app.storage.drop()
+    # Reset the database
+    db = Database()
+    db.client.drop_database(db.db.name)
 
-    # Send a message to confirm that the database has been deleted
-    client.send_message(chat_id=message.chat.id, text="The database has been deleted.")
-
+    # Send a message to confirm that the database has been reset
+    client.send_message(
+        chat_id=message.chat.id,
+        text="Database reset."
+    )
 
 if __name__ == "__main__":
     # Start the bot
