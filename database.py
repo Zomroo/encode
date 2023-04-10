5bbc16318cca320f133b741ec0e3390b46341331
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 02:04:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 class Database:
-    def __init__(self, url, db_name):
-        self.client = MongoClient(url)
+    def __init__(self, mongo_url, db_name, collection_name):
+        self.client = MongoClient(mongo_url)
         self.db = self.client[db_name]
-        self.collection = self.db.images
+        self.collection = self.db[collection_name]
