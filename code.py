c8f33f93b793b9e335d9d7c5707797522cd691db
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 02:31:40 +0530

    Update code.py

--- code.py
+++ code.py
@@ -44,9 +44,7 @@ def en_command_handler(client, message):
     db.insert_document("photos", {"_id": photo_id, "file_id": file_id})
 
     # Send a reply to the user with the ID and photo
-    client.send_photo(chat_id=message.chat.id, photo=file_id, caption=f"Your photo ID is ` <code>{photo_id}</code>`", parse_mode='markdown')
-
-
+    client.send_photo(chat_id=message.chat.id, photo=file_id, caption=f"Your photo ID is ` <code>{photo_id}</code>`", parse_mode='HTML')
 
 
 
