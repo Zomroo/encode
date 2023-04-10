b08c304f13408519f543e30d89fcb589e601ddfa
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 22:30:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import schedule
 import time
+import uuid
 
 from telegram.ext import CommandHandler
-from code import updater, get_handlers
+from code import updater, get_handlers, start_command_handler, en_command_handler, dy_command_handler
 from reset import reset_handler
 from batch import dispatcher
 from database import Database
 
 # Add the command handlers from code.py
-for handler in get_handlers():
-    dispatcher.add_handler(handler)
+dispatcher.add_handler(CommandHandler('start', start_command_handler))
+dispatcher.add_handler(CommandHandler('en', en_command_handler))
+dispatcher.add_handler(CommandHandler('dy', dy_command_handler))
 
 # Add the reset command handler
 dispatcher.add_handler(reset_handler)
@@ -23,7 +25,7 @@ def reset_database():
 schedule.every().day.at("00:00").do(reset_database)
 
 # Assign dispatcher before starting the updater
-dispatcher = updater.dispatcher
+updater.dispatcher = dispatcher
 
 # Start the bot
 updater.start_polling()
