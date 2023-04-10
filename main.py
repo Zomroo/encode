060514b767e416cc8b040fb41acb71c2ad162806
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 20:40:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,6 @@ import time
 from code import updater
 from reset import reset_handler
 from database import Database
-from batch import batch_command_handler, done_command_handler, dby_command_handler
-from existing_file import start_command_handler, en_command_handler, dy_command_handler
-
-# Add handlers for the existing commands
-start_handler = CommandHandler("start", start_command_handler)
-en_handler = CommandHandler("en", en_command_handler, filters=Filters.reply)
-dy_handler = CommandHandler("dy", dy_command_handler)
-
-# Add the handlers to the dispatcher
-updater.dispatcher.add_handler(start_handler)
-updater.dispatcher.add_handler(en_handler)
-updater.dispatcher.add_handler(dy_handler)
 
 def reset_database():
     db = Database()
