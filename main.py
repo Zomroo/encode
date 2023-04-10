b0c25590e95504afc40cd272884ac7563cdd7c09
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 19:58:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 import schedule
 import time
 
+from telegram.ext import CommandHandler
 from code import updater, get_handlers
 from reset import reset_handler
 from database import Database
 from batch import batch_command_handler, done_command_handler, dby_command_handler
-
-
-# Add the handlers to the dispatcher
-handlers = get_handlers()
-for handler in handlers:
-    updater.dispatcher.add_handler(handler)
-
-# Add the /en, /dy, and /start command handlers
-updater.dispatcher.add_handler(CommandHandler("en", en_command_handler))
-updater.dispatcher.add_handler(CommandHandler("dy", dy_command_handler))
-updater.dispatcher.add_handler(CommandHandler("start", start_command_handler))
-
 
 # Add the handlers to the dispatcher
 handlers = get_handlers()
