56589b2d17d60e587b56427b0f6e210d67749037
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 02:01:53 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ from database import Database
 from config import BOT_TOKEN, MONGO_URL
 
 # Set up the MongoDB database connection
-db = Database(MONGO_URL, MONGO_DB, MONGO_COLLECTION_NAME)
+db = Database(MONGO_URL)
 
 # Create the Pyrogram bot client
 bot = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
