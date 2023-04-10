329d87293029e2e7f389bfc9a781037e9800f1e1
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 03:51:01 +0530

    Update reset.py

--- reset.py
+++ reset.py
@@ -3,29 +3,15 @@ from pyrogram.types import Message
 from database import Database
 from config import BOT_TOKEN, AUTHORIZED_USERS
 
-# Create a Pyrogram client and pass in the bot's token
-app = Client(
-    "my_bot",
-    bot_token=BOT_TOKEN,
-    workers=1
-)
-
-# Define the reset command handler
-@app.on_message(filters.command("reset") & filters.user(AUTHORIZED_USERS))
-def reset_command_handler(client, message):
-    # Check if the user is authorized to use this command
-    if message.from_user.id not in AUTHORIZED_USERS:
-        return
-
-    # Reset the database
-    db = Database()
-    db.client.drop_database(db.db.name)
-
-    # Send a message to confirm that the database has been reset
-    client.send_message(
-        chat_id=message.chat.id,
-        text="Database reset."
-    )
+# Define the /reset command handler
+def reset(update, context):
+    if update.message.from_user.id in AUTHORIZED_USERS:
+        # Only authorized users can execute this command
+        db.drop_database('mydatabase')
+        update.message.reply_text('Database reset successful.')
+    else:
+        # Ignore the command from unauthorized users
+        update.message.reply_text('You are not authorized to use this command.')
 
 if __name__ == "__main__":
     # Start the bot
