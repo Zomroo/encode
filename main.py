9a63e26ff4732c65014a624d32ac0f712af9c90c
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 04:16:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from code import app
 
 if __name__ == "__main__":
-    # Run the bot and the reset command handler
-    app.start()
-    reset_app.run(reset_command_handler)
+    # Start the bot
+    app.run()
