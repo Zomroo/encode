cdea27182ccf9d2089faf7a942cc2037bc38ea15
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 04:01:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from code import app
-from reset import reset
+from reset import reset_app
 
 if __name__ == "__main__":
     # Run the bot and the reset command handler
-    with app:
-        app.run()
-        reset.start()
+    app.start()
+    reset_app.start()
     # Use Ctrl+C to stop the bot
