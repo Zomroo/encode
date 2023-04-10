4d8be2c5af35398c17bc11fe0c3a0c540624f62a
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-11 03:37:42 +0530

    Update reset.py

--- reset.py
+++ reset.py
@@ -2,13 +2,24 @@ import os
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.contrib import MongoStorage
 
-from config import API_ID, API_HASH, BOT_TOKEN, MONGODB_URI, MONGODB_NAME, AUTHORIZED_USERS
-from database import Database
+from config import API_ID, API_HASH, BOT_TOKEN, MONGO_URI, MONGO_DB_NAME, AUTHORIZED_USERS
 
 # Create a Pyrogram client and pass in the bot's token
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
+app = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    storage=MongoStorage(
+        uri=MONGO_URI,
+        database_name=MONGO_DB_NAME,
+        client_options={
+            "retryWrites": False
+        }
+    )
+)
 
 # Define the reset command handler
 @app.on_message(filters.command('reset') & filters.user(AUTHORIZED_USERS))
@@ -18,8 +29,7 @@ def reset_command_handler(client, message):
         return
 
     # Delete the entire database
-    db = Database(uri=MONGODB_URI, name=MONGODB_NAME)
-    db.drop_collection(MONGO_COLLECTION_NAME)
+    app.storage.drop()
 
     # Send a message to confirm that the database has been deleted
     client.send_message(chat_id=message.chat.id, text="The database has been deleted.")
