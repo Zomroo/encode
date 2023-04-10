a6b773d95bad3a29476fdb0f9a5af458092aa8a3
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-10 20:47:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import schedule
 import time
 
-from code import updater
+from code import updater, get_handlers
 from reset import reset_handler
 from database import Database
+from batch import batch_command_handler, done_command_handler, dby_command_handler
 
-def reset_database():
-    db = Database()
-    db.client.drop_database(db.db.name)
-    print("Database reset.")
+# Add handlers for the existing commands
+handlers = get_handlers()
 
-if __name__ == "__main__":
-    # start the bot
-    updater.start_polling()
+# Add the handlers to the dispatcher
+for handler in handlers:
+    updater.dispatcher.add_handler(handler)
 
-    # schedule the database reset
-    schedule.every().day.at("00:00").do(reset_database)
+# Add the batch command handlers
+updater.dispatcher.add_handler(CommandHandler("batch", batch_command_handler))
+updater.dispatcher.add_handler(CommandHandler("done", done_command_handler))
+updater.dispatcher.add_handler(CommandHandler("dby", dby_command_handler))
 
-    # add the reset command handler
-    updater.dispatcher.add_handler(reset_handler)
+# Add the reset command handler
+updater.dispatcher.add_handler(reset_handler)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+# Start the bot
+updater.start_polling()
+
+# Schedule the database reset
+schedule.every().day.at("00:00").do(Database.reset)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
